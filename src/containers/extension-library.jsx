e7f8261b8c7ab0a89a41d38c4efa978a7c7bd126
xart22 <fajar.rn22@gmail.com>
2023-05-01 23:11:37 +0700

    adding tags libary

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -1,160 +1,197 @@
-import bindAll from 'lodash.bindall';
-import PropTypes from 'prop-types';
-import React from 'react';
-import VM from 'openblock-vm';
+import bindAll from "lodash.bindall";
+import PropTypes from "prop-types";
+import React from "react";
+import VM from "openblock-vm";
 
-import analytics from '../lib/analytics';
+import analytics from "../lib/analytics";
 
-import {compose} from 'redux';
-import {connect} from 'react-redux';
-import {defineMessages, injectIntl, intlShape} from 'react-intl';
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { defineMessages, injectIntl, intlShape } from "react-intl";
 
-import extensionLibraryContent from '../lib/libraries/extensions/index.jsx';
+import extensionLibraryContent from "../lib/libraries/extensions/index.jsx";
 
-import LibraryComponent from '../components/library/library.jsx';
-import extensionIcon from '../components/action-menu/icon--sprite.svg';
+import LibraryComponent from "../components/library/library.jsx";
+import extensionIcon from "../components/action-menu/icon--sprite.svg";
 
 const messages = defineMessages({
     extensionTitle: {
-        defaultMessage: 'Choose an Extension',
-        description: 'Heading for the extension library',
-        id: 'gui.extensionLibrary.chooseAnExtension'
+        defaultMessage: "Choose an Extension",
+        description: "Heading for the extension library",
+        id: "gui.extensionLibrary.chooseAnExtension",
     },
     extensionUrl: {
-        defaultMessage: 'Enter the URL of the extension',
-        description: 'Prompt for unoffical extension url',
-        id: 'gui.extensionLibrary.extensionUrl'
+        defaultMessage: "Enter the URL of the extension",
+        description: "Prompt for unoffical extension url",
+        id: "gui.extensionLibrary.extensionUrl",
     },
     shieldTag: {
-        id: 'gui.library.shieldTag',
-        defaultMessage: 'Shield',
-        description: 'Shield tag to filter all shield libraries.'
+        id: "gui.library.shieldTag",
+        defaultMessage: "Shield",
+        description: "Shield tag to filter all shield libraries.",
     },
     actuatorTag: {
-        id: 'gui.library.actuatorTag',
-        defaultMessage: 'Actuator',
-        description: 'Actuator tag to filter all actuator libraries.'
+        id: "gui.library.actuatorTag",
+        defaultMessage: "Actuator",
+        description: "Actuator tag to filter all actuator libraries.",
     },
     sensorTag: {
-        id: 'gui.library.sensorTag',
-        defaultMessage: 'Sensor',
-        description: 'Sensor tag to filter all sensor libraries.'
+        id: "gui.library.sensorTag",
+        defaultMessage: "Sensor",
+        description: "Sensor tag to filter all sensor libraries.",
     },
     displayTag: {
-        id: 'gui.library.displayTag',
-        defaultMessage: 'Display',
-        description: 'Display tag to filter all display libraries.'
+        id: "gui.library.displayTag",
+        defaultMessage: "Display",
+        description: "Display tag to filter all display libraries.",
     },
     communicationTag: {
-        id: 'gui.library.communicationTag',
-        defaultMessage: 'Communication',
-        description: 'Communication tag to filter all communication libraries.'
+        id: "gui.library.communicationTag",
+        defaultMessage: "Communication",
+        description: "Communication tag to filter all communication libraries.",
+    },
+    machineLearningTag: {
+        id: "gui.library.machineLearningTag",
+        defaultMessage: "Machine Learning",
+        description:
+            "Machine Learning tag to filter all machine learning libraries.",
     },
     otherTag: {
-        id: 'gui.library.otherTag',
-        defaultMessage: 'Other',
-        description: 'Other tag to filter all other libraries.'
-    }
+        id: "gui.library.otherTag",
+        defaultMessage: "Other",
+        description: "Other tag to filter all other libraries.",
+    },
 });
 
-const SHIELD_TAG = {tag: 'shield', intlLabel: messages.shieldTag};
-const ACTUATOR_TAG = {tag: 'actuator', intlLabel: messages.actuatorTag};
-const SENSOR_TAG = {tag: 'sensor', intlLabel: messages.sensorTag};
-const DISPLAY_TAG = {tag: 'display', intlLabel: messages.displayTag};
-const COMMUNICATION_TAG = {tag: 'communication', intlLabel: messages.communicationTag};
-const OTHER_TAG = {tag: 'other', intlLabel: messages.otherTag};
-const tagListPrefix = [SHIELD_TAG, ACTUATOR_TAG, SENSOR_TAG, DISPLAY_TAG, COMMUNICATION_TAG, OTHER_TAG];
+const SHIELD_TAG = { tag: "shield", intlLabel: messages.shieldTag };
+const ACTUATOR_TAG = { tag: "actuator", intlLabel: messages.actuatorTag };
+const SENSOR_TAG = { tag: "sensor", intlLabel: messages.sensorTag };
+const DISPLAY_TAG = { tag: "display", intlLabel: messages.displayTag };
+const COMMUNICATION_TAG = {
+    tag: "communication",
+    intlLabel: messages.communicationTag,
+};
+const MACHINE_LEARNING_TAG = {
+    tag: "machinelearning",
+    intlLabel: messages.machineLearningTag,
+};
+const OTHER_TAG = { tag: "other", intlLabel: messages.otherTag };
+
+const tagListPrefix = [
+    SHIELD_TAG,
+    ACTUATOR_TAG,
+    SENSOR_TAG,
+    DISPLAY_TAG,
+    COMMUNICATION_TAG,
+    MACHINE_LEARNING_TAG,
+    OTHER_TAG,
+];
 
 class ExtensionLibrary extends React.PureComponent {
-    constructor (props) {
+    constructor(props) {
         super(props);
-        bindAll(this, [
-            'updateDeviceExtensions',
-            'handleItemSelect'
-        ]);
+        bindAll(this, ["updateDeviceExtensions", "handleItemSelect"]);
         this.state = {
-            deviceExtensions: []
+            deviceExtensions: [],
         };
     }
 
-    componentDidMount () {
+    componentDidMount() {
         if (this.props.isRealtimeMode === false) {
             this.updateDeviceExtensions();
         }
     }
 
-    updateDeviceExtensions () {
-        this.props.vm.extensionManager.getDeviceExtensionsList()
-            .then(data => {
+    updateDeviceExtensions() {
+        this.props.vm.extensionManager
+            .getDeviceExtensionsList()
+            .then((data) => {
                 if (data) {
-                    this.setState({deviceExtensions: data});
+                    this.setState({ deviceExtensions: data });
                 }
             });
     }
 
-    handleItemSelect (item) {
+    handleItemSelect(item) {
         const id = item.extensionId;
 
         if (this.props.isRealtimeMode) {
             let url = item.extensionURL ? item.extensionURL : id;
             if (!item.disabled && !id) {
                 // eslint-disable-next-line no-alert
-                url = prompt(this.props.intl.formatMessage(messages.extensionUrl));
+                url = prompt(
+                    this.props.intl.formatMessage(messages.extensionUrl)
+                );
             }
             if (id && !item.disabled) {
                 if (this.props.vm.extensionManager.isExtensionLoaded(url)) {
                     this.props.onCategorySelected(id);
                 } else {
-                    this.props.vm.extensionManager.loadExtensionURL(url).then(() => {
-                        this.props.onCategorySelected(id);
-                        analytics.event({
-                            category: 'extensions',
-                            action: 'select extension',
-                            label: id
+                    this.props.vm.extensionManager
+                        .loadExtensionURL(url)
+                        .then(() => {
+                            this.props.onCategorySelected(id);
+                            analytics.event({
+                                category: "extensions",
+                                action: "select extension",
+                                label: id,
+                            });
                         });
-                    });
                 }
             }
         } else if (id && !item.disabled) {
             if (this.props.vm.extensionManager.isDeviceExtensionLoaded(id)) {
-                this.props.vm.extensionManager.unloadDeviceExtension(id).then(() => {
-                    this.updateDeviceExtensions();
-                });
-            } else {
-                this.props.vm.extensionManager.loadDeviceExtension(id).then(() => {
-                    this.updateDeviceExtensions();
-                    analytics.event({
-                        category: 'extensions',
-                        action: 'select device extension',
-                        label: id
+                this.props.vm.extensionManager
+                    .unloadDeviceExtension(id)
+                    .then(() => {
+                        this.updateDeviceExtensions();
                     });
-                })
-                    .catch(err => {
+            } else {
+                this.props.vm.extensionManager
+                    .loadDeviceExtension(id)
+                    .then(() => {
+                        this.updateDeviceExtensions();
+                        analytics.event({
+                            category: "extensions",
+                            action: "select device extension",
+                            label: id,
+                        });
+                    })
+                    .catch((err) => {
                         // TODO add a alet device extension load failed. and change the state to bar to failed state
                         console.error(`err = ${err}`); // eslint-disable-line no-console
                     });
             }
         }
     }
-    render () {
+    render() {
         let extensionLibraryThumbnailData = [];
-        const device = this.props.deviceData.find(dev => dev.deviceId === this.props.deviceId);
+        const device = this.props.deviceData.find(
+            (dev) => dev.deviceId === this.props.deviceId
+        );
 
         if (this.props.isRealtimeMode) {
-            extensionLibraryThumbnailData = extensionLibraryContent.map(extension => ({
-                rawURL: extension.iconURL || extensionIcon,
-                ...extension
-            }));
-        } else {
-            extensionLibraryThumbnailData = this.state.deviceExtensions.filter(
-                extension => extension.supportDevice.includes(this.props.deviceId) ||
-                    extension.supportDevice.includes(device.deviceExtensionsCompatible))
-                .map(extension => ({
+            extensionLibraryThumbnailData = extensionLibraryContent.map(
+                (extension) => ({
                     rawURL: extension.iconURL || extensionIcon,
-                    ...extension
+                    ...extension,
+                })
+            );
+        } else {
+            extensionLibraryThumbnailData = this.state.deviceExtensions
+                .filter(
+                    (extension) =>
+                        extension.supportDevice.includes(this.props.deviceId) ||
+                        extension.supportDevice.includes(
+                            device.deviceExtensionsCompatible
+                        )
+                )
+                .map((extension) => ({
+                    rawURL: extension.iconURL || extensionIcon,
+                    ...extension,
                 }))
                 .sort((a, b) => {
-                    if ((b.isLoaded !== true) && (a.isLoaded === true)) return -1;
+                    if (b.isLoaded !== true && a.isLoaded === true) return -1;
                     return 1;
                 });
         }
@@ -184,18 +221,13 @@ ExtensionLibrary.propTypes = {
     onCategorySelected: PropTypes.func,
     onRequestClose: PropTypes.func,
     visible: PropTypes.bool,
-    vm: PropTypes.instanceOf(VM).isRequired // eslint-disable-line react/no-unused-prop-types
+    vm: PropTypes.instanceOf(VM).isRequired, // eslint-disable-line react/no-unused-prop-types
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     deviceData: state.scratchGui.deviceData.deviceData,
     deviceId: state.scratchGui.device.deviceId,
-    isRealtimeMode: state.scratchGui.programMode.isRealtimeMode
+    isRealtimeMode: state.scratchGui.programMode.isRealtimeMode,
 });
 
-export default compose(
-    injectIntl,
-    connect(
-        mapStateToProps
-    )
-)(ExtensionLibrary);
+export default compose(injectIntl, connect(mapStateToProps))(ExtensionLibrary);
